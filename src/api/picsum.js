48f622d73f480d3ce22bbf4ad18f4562441ff0da
Ihor Tian <tyanigor89@gmail.com>
2022-12-04 18:56:44 +0200

    addressed comments

--- src/api/picsum.js
+++ src/api/picsum.js
@@ -1,5 +1,5 @@
 import axios from "../helpers/axios";
 
 export function getPhotos(page = 1, limit = 10) {
-  return axios.get(`/v2/list?page=${page}&limit=${limit}`);
+  return axios.get("/v2/list", { params: { page, limit } });
 }
